d0172c9e170f71a466d5d50338d51a5bd3747044
Sujan Shrestha <Sujan.shrestha@agile.com.np>
2024-07-25 12:20:12 +0545

    tst

--- test.Server/Controllers/WeatherForecastController.cs
+++ test.Server/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace test.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchingsd"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
